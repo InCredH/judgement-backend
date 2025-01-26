d1724a84cc53e5e6b15f7d62feb116e04fbd457e
InCredH <85639817+InCredH@users.noreply.github.com>
2025-01-26 18:41:52 +0530

    finalized backend changes along with testing for complete gameflow (game restart needs to be tested

--- src/main/java/com/cardgame/judgement/service/PlayerRoundService.java
+++ src/main/java/com/cardgame/judgement/service/PlayerRoundService.java
@@ -97,4 +97,8 @@ public class PlayerRoundService {
 
         playerRoundRepository.save(playerRound);
     }
+
+    public void dropAllRecords() {
+        playerRoundRepository.deleteAll();
+    }
 }
